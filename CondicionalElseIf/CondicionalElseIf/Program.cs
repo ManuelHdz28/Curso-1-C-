88a63b0fa5645d3ed83d5a50531177dffca48c0b
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 16:04:31 -0600

    ejemplo de la condicional else if

---
+++ CondicionalElseIf/CondicionalElseIf/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // CONDICIONAL ELSEIF
+
+        Console.WriteLine("Ingresa tu Edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad <= 0)
+        {
+            Console.WriteLine("La edad no es valida");
+        }else if (edad <= 12)
+        {
+            Console.WriteLine("Eres un niño");
+        }else if(edad <= 18)
+        {
+            Console.WriteLine("Eres un Adolecente");
+        }else if (edad <= 30)
+        {
+            Console.WriteLine("Eres un adulto joven");
+        }else
+        {
+            Console.WriteLine("eres un adulto");
+        }
+    }
+}
