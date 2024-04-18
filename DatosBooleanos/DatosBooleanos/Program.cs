3151a319ce1c385bf6e471d1a2af230e4a9aab39
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 15:02:36 -0600

    Add files via upload

---
+++ DatosBooleanos/DatosBooleanos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //booleano
+
+        bool esEstudiante = false;
+        bool tieneDescuento = true;
+
+        Console.WriteLine("situacion:");
+        Console.WriteLine("Es estudiante: " + esEstudiante);
+        Console.WriteLine("Tiene descuento: " + tieneDescuento);
+    }
+}
