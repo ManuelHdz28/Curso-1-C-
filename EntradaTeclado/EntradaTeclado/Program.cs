ef9a393254279efb9692e5b0ac65facd87f31705
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-17 18:21:51 -0600

    nuevo ejercicio subida de datos

--- EntradaTeclado/EntradaTeclado/Program.cs
+++ EntradaTeclado/EntradaTeclado/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        //entrada de teclado
+        Console.WriteLine("Ingresa tu nombre: ");
+        string nombre = Console.ReadLine();//para que el usuario escriba
+
+        Console.WriteLine("ingresa tu edad: ");
+        int edad =  Convert.ToInt32(Console.ReadLine());// los tipos numericos se tienen que convertir antes
+
+        
+        Console.WriteLine("hola " + nombre + " Tienes " + edad + " años.");
+        
+    }
+}
