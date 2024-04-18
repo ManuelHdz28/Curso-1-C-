ce1f22db7ca82190b26444dd77993065fc503171
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 15:20:08 -0600

    Add files via upload

---
+++ OperadoresRelacionales/OperadoresRelacionales/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // operadores relacionales sirven para comparaciones
+
+        int edad = 20; // asignacion de valor
+
+        Console.WriteLine(1 > 2);
+        Console.WriteLine(1 < 2);
+        Console.WriteLine(1 >= 1);
+        Console.WriteLine(1 <= 2);
+        Console.WriteLine(10 == 10); // igualacion de valores
+        Console.WriteLine(10 != 9);  //diferenciacion
+
+    }
+}
