95835de7d377d36d799eed2acb6e61d793bee6cf
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 14:22:36 -0600

    ejercicio sencillo de factura de compras

---
+++ Ejercicio6FacturadeCompras/Ejercicio6FacturadeCompras/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*
+         Implementa un programa que simule una factura de compra. Solicita al usuario el nombre del artículo, 
+         la cantidad y el precio unitario. 
+         Calcula el monto total y muestra una factura detallada en la consola.
+         */
+
+        Console.WriteLine("Digite el nombre del articulo:");
+        string nombreArticulo = Console.ReadLine();
+        Console.WriteLine("Digite la cantidad de articulos: ");
+        int cantidadArticulos = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite el precio unitario del articulo: ");
+        double precioUnitario = Convert.ToDouble(Console.ReadLine());
+
+        double valorTotal = precioUnitario * cantidadArticulos;
+
+        Console.WriteLine("el valor total de " + nombreArticulo + " es: " + valorTotal);
+    }
+}
