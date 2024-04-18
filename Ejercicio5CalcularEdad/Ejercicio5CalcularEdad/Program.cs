23cfaf57aad73d8a11a5fcc2a5ced1bdc4a0850e
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 14:10:45 -0600

    ejercicio calcular la edad usando el año de nacimiento

---
+++ Ejercicio5CalcularEdad/Ejercicio5CalcularEdad/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*
+         Desarrolla un programa que solicite al usuario su nombre y su año de nacimiento. 
+         Calcula la edad actual y muestra un mensaje personalizado con el nombre y la edad.
+         */
+
+        Console.WriteLine("hola, Digite su nombre: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine(nombre + " digite su año de nacimiento: ");
+        int nacimiento = Convert.ToInt32(Console.ReadLine());
+
+        int edad = 2024 - nacimiento;
+        
+        Console.WriteLine(nombre + " tu edad es: " +  edad);
+    }
+}
