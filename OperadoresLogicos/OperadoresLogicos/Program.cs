921523b502596cd0f58b83d65c0d8bc8d6d1149d
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 15:28:30 -0600

    operadores logicos

---
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //OPERADORES LOGICOS
+        Console.WriteLine(1 < 2 && 10 != 10); // && es un operador logico AND
+        Console.WriteLine(1 < 2 || 5 == 0); // || es un operador logico OR
+
+    }
+}
