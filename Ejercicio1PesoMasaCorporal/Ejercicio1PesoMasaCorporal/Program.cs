8f01e9c07973b7f60e628a1a69c83b97e95109ea
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-17 18:05:08 -0600

    Add files via upload

---
+++ Ejercicio1PesoMasaCorporal/Ejercicio1PesoMasaCorporal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        float peso = 70;
+        double altura = 1.75;
+        double imc;
+
+        imc = peso / (altura * altura);
+        Console.WriteLine("su masa corporal es de: " + imc);
+    }
+}
---
+++ Ejercicio2ConvertirTemperatura/Ejercicio2ConvertirTemperatura/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double celcius = 25;
+
+        double fahrenheit = (celcius * 9 / 5) + 32;
+
+        
+        Console.WriteLine("la conversion de celcius a fahrenhein es: " + fahrenheit);
+    }
+}
+
+
