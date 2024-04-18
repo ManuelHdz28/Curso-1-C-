e2c20673b6bb00ed82bfad8e909402cc3050b01e
Manuel Hernandez <114449943+ManuelHdz28@users.noreply.github.com>
2024-04-18 15:50:21 -0600

    uso de condicionales IF y ELSE

---
+++ CondicionalesIfElse/CondicionalesIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //condicional IF y ELSE
+        Console.WriteLine("Ingrese su edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18) {
+            Console.WriteLine("Eres Mayor de Edad. Puedes Acceder");
+        }else { Console.WriteLine("Erese Menor de Edad. No Puedes Acceder"); }
+    }
+}
